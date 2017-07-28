d85d475664d8cb9436f4dc861cad4c264e29b002
Dan Grebb <dan@dgrebb.com>
2017-07-27 21:41:56 -0400

    add lodash tests file

---
+++ app/_components/_lodash-tests.js
@@ -0,0 +1,11 @@
+// MOCKS
+
+var numberArray = [1, 3, 7, 23, 634, 234, 4, 11, 19];
+
+const loIndexOf = () => {
+    console.log('loIndexOf');
+
+    console.log(_.indexOf(numberArray, 23));
+}
+
+export { loIndexOf };
